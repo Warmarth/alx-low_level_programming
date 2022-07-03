4fd0d2a567a0808ed2d89fb977279121ae7a8eff
Warmarth <godsonibeaka@gmail.com>
2022-07-03 12:39:26 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,17 @@
 int main(void)
 {
 int p, q;
-for (p = 0; p <= 99; p++)
+for (p = 0; p <= 98; p++)
 {
 for (q = p + 1; q <= 99; q++)
 {
-if (p != q)
-putchar(p / 10 + 48);
-putchar(p % 10 + 48);
+putchar(p / 10 + 0);
+putchar(p % 10 + 0);
 putchar(' ');
-putchar(q / 10 + 48);
-putchar(q % 10 + 48);
-if (p * 100 + q != 9899)
-
+putchar(q / 10 + 0);
+putchar(q % 10 + 0);
+if (p == 98 && q == 99)
+continue;
 putchar(',');
 putchar(' ');
 
