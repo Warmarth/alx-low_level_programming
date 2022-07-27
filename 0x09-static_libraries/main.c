f409470263aa3ab5a0754ce48fcf520fafc04448
Warmarth <godsonibeaka@gmail.com>
2022-07-27 03:23:43 -0700

    Task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Godsgift");
-	return (0);
+_puts("\"At the end of the day, my goal is be the best hacker\"\n\t- sombi");
+return (0);
 }
