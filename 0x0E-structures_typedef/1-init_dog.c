6557cde9556480997ee5a0532ab46388c8ba09b7
Warmarth <godsonibeaka@gmail.com>
2022-08-02 09:03:42 -0700

    correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return;
+d = malloc(sizeof(struct dog));
+if (d == NULL)
+	return;
 }
 d->name = name;
 d->age = age;
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,16 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-	printf("Name: ");
-	if (d->name == NULL)
+printf("Name: ");
+if (d->name == NULL)
 		printf("(nil)\n");
-	else
-		printf("%s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: ");
-	if (d->owner == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->owner);
+else
+	printf("%s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: ");
+if (d->owner == NULL)
+	printf("(nil)\n");
+else
+	printf("%s\n", d->owner);
 }
 }
