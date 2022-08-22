896cb453278d38248c2894f616f94ebb9dd5f9ce
Warmarth <godsonibeaka@gmail.com>
2022-08-22 16:16:11 -0700

    Task 0 & 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-  * read_textfile - Entry Point
-  * @filename: file name
-  * @letters: size
-  * Return: 0
-  */
+ * read_textfile - reads and prints content of a file
+ * @filename: file name
+ * @letters: number of letters to print
+ *
+ * Return: Number of chars printed or 0
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int file, rd, wr;
-char *buf;
-if (filename == NULL)
+int fd, retval, tmp;
+char *buff;
+if (!filename)
 	return (0);
-file = open(filename, O_RDONLY);
-if (file == -1)
+buff = malloc(sizeof(char) * letters);
+if (!buff)
 	return (0);
-buf = malloc(sizeof(char) * letters + 1);
-if (buf == NULL)
+fd = open(filename, O_RDONLY);
+tmp = read(fd, buff, letters);
+retval = write(1, buff, tmp);
+free(buff);
+if (fd == -1 || tmp == -1 || retval == -1 || retval != tmp)
 	return (0);
-rd = read(file, buf, letters);
-if (rd == -1)
-	return (0);
-buf[letters] = '\0';
-wr = write(1, buf, rd);
-if (wr == -1)
-	return (0);
-close(file);
-free(buf);
-return (wr);
+close(fd);
+return (retval);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * create_file - Entry Point
+ * create_file - creates a file and add content to it
  * @filename: file name
- * @text_content: null terminated string to write
- * Return: 1
+ * @text_content: content to put in the file
+ *
+ * Return: 1 for success or -1 if it fails
  */
 
 int create_file(const char *filename, char *text_content)
 {
-int file, i = 0;
-if (filename == NULL)
+int fd, i = 0, retval = 1;
+if (!filename)
 	return (-1);
-file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-if (file == -1)
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
 	return (-1);
+if (!text_content)
+{
+	close(fd);
+	return (1);
+}
 while (text_content[i])
 	i++;
-if (text_content == NULL)
-{
-	close(file);
+	retval = write(fd, text_content, i);
+	close(fd);
+if (retval == -1 || retval != i)
 	return (-1);
-}
-else
-{
-	write(file, text_content, i);
-}
-close(file);
-return (1);
-}
+	return (1);
+	}
