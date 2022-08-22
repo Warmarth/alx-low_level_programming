8b592ede2bcbb2bfc0d570c2f07813bcc3da46f6
Warmarth <godsonibeaka@gmail.com>
2022-08-22 16:35:17 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,12 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd, i = 0, retval = 1;
-if (!filename)
+if (filename == NULL)
 	return (-1);
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 	return (-1);
-if (!text_content)
+if (text_content != NULL)
 {
 	close(fd);
 	return (1);
